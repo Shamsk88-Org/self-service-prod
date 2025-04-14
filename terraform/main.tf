0b8070350b1d0216999b95add54e5135b211aca0
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-15 00:37:56 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id = "1ac098dd-90b5-4054-80e7-8baf99b04f64"
+  tenant_id = "648b628e-c17f-4d43-8193-72e3f4d7499d"
+  subscription_id = "24143a81-fcba-4b21-9b28-c910b75e6580"
 }
 
 terraform {
