8cfd7245cd57c5874519347f7c9ddba23350e758
shamsk88 <51985457+shamsk88@users.noreply.github.com>
2025-04-15 19:40:15 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = "The name of the Storage Account resource group "
+}
+
+variable "storage_account_name" {
+  description = "The name of the Storage Account"
+}
+
+variable "container_name" {
+  description = "The name of the Storage Container"
+}
+
+variable "key" {
+  description = "The name of the terraform state file"
+}
+
+variable "tenant_id" {
+  description = "The vaule of the Tenant ID"
+}
+
+variable "subscription_id" {
+  description = "The value of the Subscription ID"
+}
+
+variable "client_id" {
+  description = "The value of the Client ID"
+}
+
+variable "resource_name" {
+  description = "The name of the resource"
+}
+
+variable "location" {
+  description = "Resource Location"
+}
