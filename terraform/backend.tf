f4ed4b2df9730ec586f900c14556c9d5eaced7ba
shamsherkhan <shamsherkhan.@cognizant.com>
2025-05-08 15:14:39 +0530

    added code

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 ## Backend file #
 terraform {
   backend "azurerm" {
-    resource_group_name = var.resource_group_name
+    resource_group_name = var.resource_name
     storage_account_name = var.storage_account_name
     container_name = var.container_name
     key = var.resource_group_name
